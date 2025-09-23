b00944ce07ccb703a19a5a022eae95a09374211d
adiprajadinata81 <adiprajadinata81@gmail.com>
2025-09-23 12:24:48 +0700

    initial commit

--- latihan.js
+++ latihan.js
@@ -1,7 +1,7 @@
-function bagi(angka1, angka2){
-let hasil = angka1 / angka2;
+function tambah(angka1, angka2){
+	let hasil = angka1 + angka2);
 return hasil;
 }
 
-let jumlah = bagi(10, 5);
+let jumlah = tambah(8, 1);
 console.log(jumlah);
