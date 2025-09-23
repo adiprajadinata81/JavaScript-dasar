56b8e9096e603e82acc0c29cc79dd34660c9419e
adiprajadinata81 <adiprajadinata81@gmail.com>
2025-09-23 19:50:52 +0700

    Initial commit

--- latihan.js
+++ latihan.js
@@ -1,5 +1,5 @@
 let pembalap1 = "Oscar piastri = "
-let pembalal2 = "Max Verstappen = "
+let pembalap2 = "Max Verstappen = "
 
 let oscarPiastri = 324;
 function poin1(){
@@ -10,7 +10,5 @@ let maxVerstappen = 255;
 function poin2(){
 	return maxVerstappen += 175;
 }
-console.log(pembalap1);
-console.log(oembalap2);
-console.log(poin1());
-console.log(poin2());
+console.log(pembalap1 + poin1());
+console.log(pembalap2 + poin2());
