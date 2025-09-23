3d40c76c0a66bbd9f1b75eb0dc5959f48ab464c2
adiprajadinata81 <adiprajadinata81@gmail.com>
2025-09-23 17:34:03 +0700

    initial commit

--- latihan.js
+++ latihan.js
@@ -1,7 +1,5 @@
-function tambah(angka1, angka2){
-	let hasil = angka1 + angka2;
-return hasil;
+function theWall(){
+	return "high wall";
 }
+console.log(theWall());
 
-let jumlah = tambah(8, 1);
-console.log(jumlah);
