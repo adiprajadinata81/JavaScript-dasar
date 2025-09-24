9769e800d5d4a4b83a55e92afeca102fd5d6a6b9
adiprajadinata81 <adiprajadinata81@gmail.com>
2025-09-24 13:52:37 +0700

    initial commit

--- latihan.js
+++ latihan.js
@@ -1,5 +1,16 @@
-let namaBelakang = 'Prajadinata ';
-function nama(){
-	return 'Adi ';
+const olahraga = 'basketball';
+
+function namaAtlet(){
+ let atlet = 'lionelMessi';
+ const noMessi = 10;
+ console.log(olahraga);
+
+ if(olahraga === 'basketball'){
+ atlet = 'kobeBryant';
+ const noKobe = 24;
+ console.log(olahraga);
+ }
+ return atlet;
 }
-console.log(nama() + namaBelakang);
+console.log(namaAtlet());
+console.log(olahraga);
