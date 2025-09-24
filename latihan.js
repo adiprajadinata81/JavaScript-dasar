b56625d6c4320e5a0fcc52382fba54eeb5fc2fc3
adiprajadinata81 <adiprajadinata81@gmail.com>
2025-09-24 19:51:52 +0700

    Initial commit

--- latihan.js
+++ latihan.js
@@ -5,12 +5,12 @@ let kalimat2 = 'mempunyai Poin Balap sebanyak 255 ';
 function namaPembalap(){
  let pembalap = 'oscarPiastri';
  const noOscar = '81';
- console.log(sport);
+ 
 
  if(sport === 'formula1'){
  pembalap = 'maxVerstappen ';
  const noMax = '1';
- console.log(sport);
+ 
  }
  return pembalap;
 }
