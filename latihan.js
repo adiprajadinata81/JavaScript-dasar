ae894c994bb761766e4ee5eb02878dba10b46a78
adiprajadinata81 <adiprajadinata81@gmail.com>
2025-09-24 19:42:04 +0700

    initial commit

--- latihan.js
+++ latihan.js
@@ -1,5 +1,16 @@
-let atletName = 'maxVerstappen';
-function displayAtletName(){
-	return 'Nama atlet adalah ' + atletName;
+const sport = 'formula1';
+
+function namaPembalap(){
+ let pembalap = 'oscarPiastri';
+ const noOscar = '81';
+ console.log(sport);
+
+ if(sport === 'formula1'){
+ pembalap = 'maxVerstappen';
+ const noMax = '1';
+ console.log(sport);
+ }
+ return pembalap;
 }
-console.log(displayAtletName());
+console.log(namaPembalap());
+console.log(sport);
