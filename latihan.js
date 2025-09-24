ede0d3c87adbeae90e1974ce3c39283b48270794
adiprajadinata81 <adiprajadinata81@gmail.com>
2025-09-24 10:48:50 +0700

    initial commit

--- latihan.js
+++ latihan.js
@@ -1,19 +1,3 @@
-const olahraga = 'basketball';
-let nomorPunggung = 'nomor Punggung saya adalah ';
-let nama = 'nama saya adalah ';
-
-function namaAtlet(){
- let atlet = 'lionelMessi';
- const noMessi = 10;
- console.log(nama + atlet);
-
-if(olahraga === 'basketball'){
- atlet = 'kobeBryant';
- const noKobe = 24;
- console.log(nomorPunggung + noKobe);
- console.log(nama + atlet);
- }
- return atlet;
-}
-console.log(namaAtlet());
-
+let tertawa = 'tapiTerluka';
+let apakahTerluka = tertawa === 'tapiTerluka' ? 'Tertawa Tapi Terluka' : 'Tertawa';
+console.log(apakahTerluka);
