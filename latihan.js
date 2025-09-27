87a88c3b3ed51a39d01b084369d7cd35ffa8ca2f
adiprajadinata <adiprajadinata81@gmail.com>
2025-09-27 22:50:36 +0700

    Update latihan.js
    Initial Commit.

--- latihan.js
+++ latihan.js
@@ -1,19 +1,4 @@
-let identitas = 'Nama saya ';
-let umur = 'umur saya ';
-let tahun = ' Tahun ';
-let orang = {
-	nama: 'adi ',
-	umur: 18 ,
-	pekerjaan: 'programming'
-};
-
-let motor = new Object();
-nama: 'vario';
-tipe:'matic';
-warna: 'hitam';
-
-let saya = new Object();
-nama: 'adi';
-umur: 18 ;
-
-console.log(identitas + orang.nama + umur + orang.umur + tahun);
+function nama(){
+	return 'Adi Prajadinata';
+}
+console.log(nama());
