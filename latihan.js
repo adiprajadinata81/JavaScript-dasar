59a567bdc20a2793bb91ff72499604ef870d471c
adiprajadinata81 <adiprajadinata81@gmail.com>
2025-10-10 09:29:49 +0700

    initial commit

--- latihan.js
+++ latihan.js
@@ -1,24 +1,8 @@
-const user = {
-   nama: "Stefan",
-   umur: 21,
-   makananFavorit: "Pizza",
-   perkenalanDiri: () => { 
-      return `Nama saya ${this.nama}. Saya berumur ${this.umur}. Saya suka ${this.makananFavorit}`;
-   }
+let kalkulator = {
+  namaOperasi: 'penjumlahan',
+  jumlah: function(angka1, angka2) {
+    return angka1 + angka2;
+  }
 };
 
-// panggil method tersebut di dalam fungsi yang kita inginkan
-function fungsiA() {
-  // isi dari fungsiA
-  console.log(user.perkenalanDiri());
-}
-
-function fungsiB() {
-  // isi dari fungsiB
-  console.log(user.perkenalanDiri());
-}
-
-function fungsiC() {
-  // isi dari fungsiC
-  console.log(user.perkenalanDiri());
-}
+console.log(kalkulator.jumlah(2, 3));
