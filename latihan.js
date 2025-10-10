adb39feb441093af3771e0647a91e0a5ec10572b
adiprajadinata81 <adiprajadinata81@gmail.com>
2025-10-10 09:36:42 +0700

    initial commit

--- latihan.js
+++ latihan.js
@@ -2,7 +2,12 @@ let kalkulator = {
   namaOperasi: 'penjumlahan',
   jumlah: function(angka1, angka2) {
     return angka1 + angka2;
+  },
+  jumlah2: function(angka3, angka4){
+    return angka3 * angka4;
   }
 };
 
+console.log(kalkulator.jumlah2(3, 3));
 console.log(kalkulator.jumlah(2, 3));
+console.log(kalkulator.namaOperasi);
