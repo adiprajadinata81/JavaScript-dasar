74b3fd90dd26482b559060c73258fb4763042163
adiprajadinata81 <adiprajadinata81@gmail.com>
2025-10-11 08:05:14 +0700

    initial commit

--- latihan.js
+++ latihan.js
@@ -1,9 +1,14 @@
-let namaBilangan = 'hasil dari penjumlahan 8 ditambah 1 adalah = ';
-let kalkulator = {
- namaOperasi: 'penjumlahan',
- bilangan: function(angka1, angka2){
-  return angka1 + angka2;
+const anjing = {
+	suara: function(){
+  return 'GukGuk';
  }
 };
-console.log(kalkulator.namaOperasi);
-console.log(namaBilangan + kalkulator.bilangan(8, 1));
+
+const kucing = {
+	suara: function(){
+  return 'Meong';
+ }
+};
+
+console.log(anjing.suara());
+console.log(kucing.suara());
