74a5845e7960f31f1c603d2a246a5b9d4d0580dd
adiprajadinata81 <adiprajadinata81@gmail.com>
2025-10-12 09:56:33 +0700

    initial commit

--- latihan.js
+++ latihan.js
@@ -1,10 +1,10 @@
 let kucing = {
-	lucu: true,
-	kaki: 4
+	kaki: 4,
+	lucu: true
 };
 
 kucing.suara = function suaraKucing(){
 	return 'Meong';
 };
-
 console.log(kucing);
+console.log(kucing.suara());
