af9aa7ab521879321e9125383a58483c55d825d4
adiprajadinata81 <adiprajadinata81>
2025-11-02 06:41:24 +0700

    initial commit

--- latihan.js
+++ latihan.js
@@ -1,9 +1,3 @@
-let buah = ['mangga', 'apel', 'jeruk'];
-let hargaBuah = ['20000', 30000, 15000];
+let kucing = 'meong';
+console.log(kucing);
 
-console.log(buah[0]);
-console.log(hargaBuah[0]);
-console.log(buah[buah.length - 1]);
-console.log(buah);
-console.log(hargaBuah);
-console.log(buah.length);
