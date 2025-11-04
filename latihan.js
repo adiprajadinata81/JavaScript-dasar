bfd54efdd4ff2156a46d4c20a6e1ccf026dfac7b
adiprajadinata81 <adiprajadinata81>
2025-11-04 14:34:31 +0700

    initial commit

--- latihan.js
+++ latihan.js
@@ -3,5 +3,11 @@ function namaAtlet(){
   let atlet = 'lionelMessi';
   const noMessi = 10;
   console.log(olahraga);
+  if(olahraga === 'basketball'){
+  atlet = 'kobeBryant';
+  noKobe = 24;
+  console.log(olahraga);
+  }
+  return atlet;
 }
-
+console.log(atlet());
