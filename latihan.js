220d762a686c0de1c7ba9804182dbcdd7c0eeb99
adiprajadinata <adiprajadinata81@gmail.com>
2025-11-06 14:09:22 +0700

    Refactor athlete logic and add fruit array
    Removed the athlete selection logic and added a fruit array.

--- latihan.js
+++ latihan.js
@@ -1,14 +1,2 @@
-const olahraga = 'basketball';
-function namaAtlet(){
-  let atlet = 'lionelMessi';
-  const noMessi = 10;
-  console.log(olahraga);
-  if(olahraga === 'basketball'){
-    atlet = 'kobeBryant';
-    const noKobe = 24;
-    console.log(olahraga);
-  }
-  return atlet;
-}
-console.log(namaAtlet());
-console.log(olahraga);
+let namaBuah = ['apel', 'jeruk', 'mangga', 'pisang'];
+console.log(namaBuah[2]);
