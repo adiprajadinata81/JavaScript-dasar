71dd7ef20fc56ccfcdac0102646780b5eabfc8de
adiprajadinata <adiprajadinata81@gmail.com>
2025-11-06 13:53:35 +0700

    Change athlete name to Kobe if sport is basketball
    Update athlete name based on sport condition and add variable for Kobe.

--- latihan.js
+++ latihan.js
@@ -4,6 +4,11 @@ function namaAtlet(){
   const noMessi = 10;
   console.log(olahraga);
   if(olahraga === 'basketball'){
-    atlet
+    atlet = 'kobeBryant';
+    const noKobe = 24;
+    console.log(olahraga);
   }
+  return atlet;
 }
+console.log(namaAtlet());
+console.log(olahraga);
