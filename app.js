302ed3a10ec4af7e9f9711b19978ec23c8cb4152
adiprajadinata81 <adiprajadinata81@gmail.com>
2025-11-08 15:00:45 +0700

    initial commit

--- app.js
+++ app.js
@@ -1,27 +1,5 @@
-let bilangan = [30, 1, 2, 3.5, 10, 100];
-
-
-let urutDariTerkecil = function(a, b) {
-  return a - b;
+let bilangan = [2,3,1,4,6,5,8,7,10,9];
+let urutanDariTerkecil = function(a, b){
+    return a - b;
 };
-
-let urutDariTerbesar = function(a, b) {
-  return b - a;
-};
-
-console.log(bilangan.sort(urutDariTerkecil)); 
-console.log(bilangan.sort(urutDariTerbesar));
-let isSortedDesc = true;
-
-for (let i = 1; i < bilangan.length; i++) {
-  if (bilangan[i - 1] < bilangan[i]) {
-    isSortedDesc = false;
-    break;
-  }
-}
-
-if (isSortedDesc) {
-  console.log("Array sudah urut dari terbesar ke terkecil");
-} else {
-  console.log("Array TIDAK urut dari terbesar ke terkecil");
-}
+console.log(bilangan.sort(urutanDariTerkecil));
