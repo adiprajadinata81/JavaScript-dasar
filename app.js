9f9c185c9ffd9c9b9f029172f93a6dd09c0a4b3a
adiprajadinata81 <adiprajadinata81@gmail.com>
2025-11-08 14:12:11 +0700

    initial commit

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-let namaBuah = ['pisang', 'apel'];
-console.log(namaBuah);
-console.log(namaBuah[1]);
+let cemilan = ["kripik singkong", "kripik kentang", "krupuk ikan", "permen", "coklat", "kue"];
+let cemilanGurih = cemilan.slice(0, 1);
+let cemilanManis = cemilan.slice(2);
+
+console.log(cemilanGurih); 
+console.log(cemilanManis);
