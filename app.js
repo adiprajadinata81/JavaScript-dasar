0ff8167ba0046cebdc5464fc280ee5507f5b57bb
adiprajadinata81 <adiprajadinata81@gmail.com>
2025-11-08 16:04:36 +0700

    initial commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let bilangan1 = 5.7;
-let bilangan2 = 5.0;
+let bilangan2 = 5.5;
 
-console.log(Math.ceil(bilangan1)); 
-console.log(Math.ceil(bilangan2));
+console.log(Math.round(bilangan1)); // Output: 6
+console.log(Math.round(bilangan2));
