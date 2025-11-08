7fe5f36886754e5f96dd4312437f11c7c0ed0e6e
adiprajadinata81 <adiprajadinata81@gmail.com>
2025-11-08 15:19:13 +0700

    initial commit

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-let bilangan = [2,3,1,4,6,5,8,7,10,9];
-let urutanDariTerkecil = function(a, b){
-    return a - b;
-};
-console.log(bilangan.sort(urutanDariTerkecil));
-
-let bilanganLain = [2,3,1,4,6,5,8,7,10,9];
-bilanganLain.sort(function(a, b){
+let bilanganTerkecil = [2,1,3,5,4,8,7,6,9,10];
+bilanganTerkecil.sort(function(a, b){
     return a - b;
 });
-console.log(bilanganLain.sort(urutanDariTerkecil));
+console.log(bilanganTerkecil);
