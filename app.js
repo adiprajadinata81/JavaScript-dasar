95fa5a9b90608e9fb21b83c7521132eb3d00f355
adiprajadinata81 <adiprajadinata81@gmail.com>
2025-12-04 18:29:02 +0700

    initial commit

--- app.js
+++ app.js
@@ -1,14 +1,1 @@
-const olahraga = 'basketball';
-function namaAtlet(){
-    let atlet = 'lionelMessi';
-    const noMessi = 10;
-    console.log(olahraga);
-    if(olahraga === 'basketball'){
-    atlet = 'kobeBraynt';
-    const noKobe = 24;
-    console.log(olahraga);
-    };
-    return atlet;
-};
-console.log(namaAtlet());
-console.log(olahraga);
+console.log("(◕‿↼)");
