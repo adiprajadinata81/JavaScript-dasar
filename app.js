da0083a04ced159bfaf71a1c196511b0af15682c
adiprajadinata81 <adiprajadinata81@gmail.com>
2025-12-06 18:19:18 +0700

    initial commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const userId = "1618033988";
 let userName = "Leeroy Jenkins";
-const userId = '163344'
+userName = "leroy sane";
 console.log(userId);
 console.log(userId);
+console.log(userName);
 console.log(userName);
