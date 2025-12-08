b81028159a97f1cb1bfc1ba061ddde23a27e58c4
adiprajadinata81 <adiprajadinata81@gmail.com>
2025-12-08 11:54:13 +0700

    initial commit

--- app.js
+++ app.js
@@ -1,7 +1,2 @@
-const userId = "1618033988";
-let userName = "Leeroy Jenkin";
-userName = "leroy sane";
-console.log(userId);
-console.log(userId);
-console.log(userName);
-console.log(userName);
+let guardianTales = 'garam';
+console.log(guardianTales);
