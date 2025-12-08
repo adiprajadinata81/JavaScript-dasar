e0219e29f951f2876aaa9a6ada41b650a61f6a17
adiprajadinata81 <adiprajadinata81@gmail.com>
2025-12-08 12:25:00 +0700

    initial commit

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ const firstName = 'adi';
 const favoriteColor = 'orange';
 let curentLocation = 'indonesia';
 let mood = 'good';
+console.log(firstName);
+console.log(favoriteColor);
+console.log(curentLocation);
+console.log(mood);
