0b6efae3047852c2f872fa06f3149ff925430979
adiprajadinata81 <adiprajadinata81@gmail.com>
2026-01-01 17:13:51 +0700

    initial commit

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-let greeting = 'hello';
-console.log("hello world");
+let email = "adiprajadinata81, adiprajadinata3, adiprajadinata.last";
+let password = "printAdiprajadinata,";
+let mega = "adiprajadinata33";
+console.log(email);
+console.log(password);
+console.log(mega);
